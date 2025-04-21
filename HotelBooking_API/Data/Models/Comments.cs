0cf988bc9e639190347b4aefdd335a07a59876c2
Камаль Сарваров <sarvarov02052004@mail.ru>
2025-04-21 13:35:00 +0500

    Test comment

--- HotelBooking_API/Data/Models/Comments.cs
+++ HotelBooking_API/Data/Models/Comments.cs
@@ -9,6 +9,19 @@ namespace HotelBooking_API.Data.Models
         public int UserId { get; set; }
         public int Rating { get; set; }
         public string Text { get; set; }
-        public string TypeComent { get; set; }
+        public int? HotelId { get; set; }
+        public int? RoomId { get; set; }
+
+        public Hotel? Hotel { get; set; }
+        public Room? Room { get; set; }
+    }
+    public class NewCommentDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int Rating { get; set; }
+        public string Text { get; set; }
+        public int? HotelId { get; set; }
+        public int? RoomId { get; set; }
     }
 }
